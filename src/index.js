34a3f05d29de19d0730ff3b642d526dc2bf8a7b0
Leonardo Andrade <leo.sbrf@gmail.com>
2018-11-04 23:02:40 -0300

    (fix): adds environment variables

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import registerServiceWorker from './registerServiceWorker';
 // const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ 
 //     && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({ 
         burgerBuilder: burgerBuilderReducer, 
